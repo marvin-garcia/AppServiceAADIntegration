aa08ccd11679c66453c1b68f0c9227ef1653635c
Marvin Garcia <magar@microsoft.com>
2019-05-08 09:46:04 -0500

    added scripts and images for README reference

---
+++ Scripts/UpdateWebsiteAuthSettings.ps1
@@ -0,0 +1,17 @@
+﻿$resourceGroupName = ''
+$resourceName = '<site-name>/authsettings'
+$resourceType = 'Microsoft.Web/sites/config'
+$backendApiId = ''
+$apiVersion = '2018-02-01'
+
+# Get website auth settings
+$resource = Invoke-AzureRmResourceAction -ResourceGroupName $resourceGroupName -ResourceType $resourceType -ResourceName $resourceName -Action list -ApiVersion $apiVersion -Force
+
+# Add backend API token to login parameters
+$resource.properties.additionalLoginParams = @{
+    "response_type" = "code id_token"
+    "resource" = $backendApiId
+}
+
+# Update website
+New-AzureRmResource -PropertyObject $resource.properties -ResourceGroupName $resourceGroupName -ResourceType $resourceType -ResourceName $resourceName -ApiVersion $apiVersion -Force
---
+++ Scripts/create-app-services.ps1
@@ -0,0 +1,13 @@
+﻿$resourceGroupName = ''
+$appServicePlanName = ''
+$appServicePlanSku = '' # Allowed values: B1, B2, B3, D1, F1, FREE, P1, P1V2, P2, P2V2, P3, P3V2, PC2, PC3, PC4, S1, S2, S3, SHARED. Default: B1.
+$backendSiteName = ''
+$frontendSiteName = ''
+$location = ''
+
+az group create --name $resourceGroupName --location $location
+
+az appservice plan create --name $appServicePlanName --resource-group $resourceGroupName --sku $appServicePlanSku
+
+az webapp create --resource-group $resourceGroupName --plan $appServicePlanName --name $frontendSiteName
+az webapp create --resource-group $resourceGroupName --plan $appServicePlanName --name $backendSiteName
